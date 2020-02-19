12715f3eacb40060a82d742ecb679dce114a4aa9
David Squire <squiredd@gmail.com>
2020-02-18 21:58:57 -0800

    add service provider to execution context

---
+++ Source/Rice.Module.Abstractions/Execution/IServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rice.Module.Abstractions.Execution
+{
+    public interface IServiceProvider
+    {
+        object Resolve(Type type);
+
+        T Resolve<T>() => (T)Resolve(typeof(T));
+    }
+}
